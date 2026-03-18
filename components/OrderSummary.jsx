e81024811020789decd5b8b0f6656d62c18d9c43
Alex Mario Kristian <alexmario435@gmail.com>
2026-03-18 10:14:37 +0700

    Stripe Webhooks-Added

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -14,7 +14,7 @@ const OrderSummary = () => {
     cartItems,
     setCartItems,
   } = useAppContext();
-  
+
   const [selectedAddress, setSelectedAddress] = useState(null);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
@@ -77,6 +77,45 @@ const OrderSummary = () => {
         toast.success(data.message);
         setCartItems({});
         router.push("/order-placed");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const createOrderStripe = async () => {
+    try {
+      if (!selectedAddress) {
+        return toast.error("Please select an address");
+      }
+
+      let cartItemsArray = Object.keys(cartItems).map((key) => ({
+        product: key,
+        quantity: cartItems[key],
+      }));
+      cartItemsArray = cartItemsArray.filter((item) => item.quantity > 0);
+
+      if (cartItemsArray.length === 0) {
+        return toast.error("Cart is empty");
+      }
+
+      const token = await getToken();
+
+      const { data } = await axios.post(
+        "/api/order/stripe",
+        {
+          address: selectedAddress._id,
+          items: cartItemsArray,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        },
+      );
+
+      if (data.success) {
+        window.location.href = data.url;
       } else {
         toast.error(data.message);
       }
@@ -199,7 +238,7 @@ const OrderSummary = () => {
       </div>
 
       <button
-        onClick={createOrder}
+        onClick={createOrderStripe}
         className="w-full bg-orange-600 text-white py-3 mt-5 hover:bg-orange-700"
       >
         Place Order
